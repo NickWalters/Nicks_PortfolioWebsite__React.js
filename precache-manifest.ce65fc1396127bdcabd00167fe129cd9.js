54044340ffe5ccc6d4844a4ddeb069d3381c9bc7
Nick Walters <nicho.walters@gmail.com>
2020-06-18 01:35:53 +0800

    Updates

---
+++ precache-manifest.ce65fc1396127bdcabd00167fe129cd9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8485f580dec4f202979f43bd6c9ed2b",
+    "url": "/Nicks_PortfolioWebsite__React.js/index.html"
+  },
+  {
+    "revision": "e91ccfa588e508c93e1d",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/css/main.3586f058.chunk.css"
+  },
+  {
+    "revision": "54868c101dfa2a678457",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/js/2.4ae2f8a9.chunk.js"
+  },
+  {
+    "revision": "e91ccfa588e508c93e1d",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/js/main.605c88cb.chunk.js"
+  },
+  {
+    "revision": "87600c7ef5dc6bf96821",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/js/runtime-main.13c5a742.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e0ba9c7825dd1abf254ed77212c19abc",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/circle-cropped.e0ba9c78.png"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "a8125658aeadde9e01edb2c3a8e3f882",
+    "url": "/Nicks_PortfolioWebsite__React.js/static/media/uwa.a8125658.jpg"
+  }
+]);
